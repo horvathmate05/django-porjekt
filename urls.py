1bb7bed8f7e9336402b760323590583c4fdbdc5d
horvathmate05 <90699410+horvathmate05@users.noreply.github.com>
2022-12-31 11:28:18 +0100

    Delete urls.py

--- urls.py
+++
@@ -1,9 +0,0 @@
-a  django -tól . contrib  import  admin
-a  django -tól . URL -ek  importálási  útvonala , tartalmazza
-#from django.conf.urls importálás tartalmazza: url
-
-urlpatterns  = [
-    elérési út ( 'admin/' , admin . site . urls ),
-    elérési út ( r'' , include ( 'blog.urls' )),
-    elérési út ( "accounts/" , include ( "django.contrib.auth.urls" )),   # new
-]
